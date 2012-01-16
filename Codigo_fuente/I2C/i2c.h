5747280efdcb325f0554da8d31b493b0c0f7563e
mecmato <mecmato@gmail.com>
2012-01-16 21:33:03 -0200

    Actualizo el .h con las variables correctas

--- Codigo_fuente/I2C/i2c.h
+++ Codigo_fuente/I2C/i2c.h
@@ -5,7 +5,7 @@
 
 typedef void (*on_i2c_rcv)(int, int, char*);
 
-int i2c_transmit (int dest, char* data);
+int i2c_transmit (unsigned char destino, unsigned int largo, char* data);
 int i2c_set_rcv_callback (on_i2c_rcv cb);
 
 
